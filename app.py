23f79d686120159d4ff1fef0ea899ccfa0631109
Noha El-Haj <elhajn02@gmail.com>
2025-10-16 19:40:31 -0400

    add page routes

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
 
+@app.route("/publications")
+def publications():
+    return render_template("publications.html")
+
+@app.route("/experience")
+def experience():
+    return render_template("experience.html")
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
